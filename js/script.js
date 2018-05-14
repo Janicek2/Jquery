03c9662b3d630d307b7e0f8636f1574e79457583
Janicek2 <bjanicki82@gmail.com>
2018-05-14 14:38:09 +0200

    Add jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function() {
-    
+    var carousele = $('carousele ul');
+    setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
+    carousele.animate({'marginLeft':-200}, 500, moveFirstSlide);
+    var firstItem = carousele.find('li:first');
+    var lastItem = carousele.find('li:last');
+    lastItem.after(firstItem)
+    carousele.css({marginLeft:0});
 });
