d60924109c5f110887d10c31e33521c2dfc33187
Janicek2 <bjanicki82@gmail.com>
2018-05-15 21:42:33 +0200

    Try Arrows

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-$(document).ready(function() {
-    var carouseleList = $('ul');
-    setInterval(changeSlide, 3000);
-    function changeSlide() {
-        carouseleList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-    }
+/*$(document).ready(function() {
+    
+    //setInterval(changeSlide, 3000);
+    //function changeSlide() {
+    //    carouseleList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    //}*/
     function moveFirstSlide() {
         var firstItem = $('ul').find('li:first');
         var lastItem = $('ul').find('li:last');
         lastItem.after(firstItem);
         carouseleList.css({marginLeft:0});
-        }
-    });
+    //});
+    //}});
+    var carouseleList = $('ul');
+    var leftArrow = $('.icon1');
+    var rightArrow = $('.icon2');
+    leftArrow.click( function() {
+        carouseleList.animate({'marginLeft':+400}, 500, moveFirstSlide);
+         console.log('left');
+       });
+     rightArrow.click( function() {
+        carouseleList.animate({'marginLeft':0}, 500, moveFirstSlide);
+         console.log('right');});
