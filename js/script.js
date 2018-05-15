5b57cfc8679785ee6b8e511257d56313669c7a86
Janicek2 <bjanicki82@gmail.com>
2018-05-15 12:11:37 +0200

    Add jquery - problem with documet.write

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-$(function() {
-    var carousele = $('carousele ul');
-    setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
-    carousele.animate({'marginLeft':-200}, 500, moveFirstSlide);
-    var firstItem = carousele.find('li:first');
-    var lastItem = carousele.find('li:last');
-    lastItem.after(firstItem)
-    carousele.css({marginLeft:0});
-});
+$(document).ready(function() {
+    var carouseleList = $('carousele ul');
+    carousele.animate({'margin left':-400}, 500, moveFirstSlide);
+    function moveFirstSlide() {
+        var firstItem = carousele.find('li:first');
+        var lastItem = carousele.find('li:last');
+        lastItem.after(firstItem);
+        carousele.css({marginLeft:0});
+        console.log("start");
+        }
+    });
